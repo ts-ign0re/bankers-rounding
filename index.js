85b2f4e430f444abe43db74ef5f1b326694213a8
Tronin Denis <tronin.at.work@ya.ru>
2017-05-02 09:44:22 +0300

    add main function

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// source http://stackoverflow.com/a/3109234
+function bankersRounding(num, decimalPlaces) {
+    var d = decimalPlaces || 0;
+    var m = Math.pow(10, d);
+    var n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
+    var i = Math.floor(n), f = n - i;
+    var e = 1e-8; // Allow for rounding errors in f
+    var r = (f > 0.5 - e && f < 0.5 + e) ?
+        ((i % 2 == 0) ? i : i + 1) :
+        Math.round(n);
+
+    return d ? r / m : r;
+}
+
+module.exports = bankersRounding;
